46d1d21d214fcadbd7a9d3064dea4a5cb7c68720
Diego Bienz <diego.bienz@stud.hslu.ch>
2018-11-24 16:57:24 +0100

    Added sample code for the gui

---
+++ ViewStuff/test.py
@@ -0,0 +1,59 @@
+import sys
+import os
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtQuick import QQuickView
+from PyQt5.QtQml import QJSValue
+
+#Just for Diego
+#os.environ['QT_QPA_PLATFORM'] = 'linuxfb:fb=/dev/fb0:size=800x600:mmSize=800x600:offset=0x0:tty=/dev/tty1'
+
+
+#This class is not really used yet. Later for data binding
+class iceController(QObject):
+    def __init__(self):
+        QObject.__init__(self)
+        self.callback = []
+
+    def dump(self):
+        print('Dump was called')
+        # print('Callback is %s' % self.callback)
+        # print(dir(self.callback))
+        # print('Callback is callable %s' % self.callback.isCallable)
+        # print('Callback is callable %s' % self.callback.isCallable())
+        for c in self.callback:
+            c.call([QJSValue('asdf')])
+        self.callback = []
+
+    @pyqtSlot(str, 'QJSValue')
+    def enqueue(self, command, callback):
+        print('Enqueuing function of %s' % command)
+        # print('Test callback is %s' % callback)
+        # print('Callback is callable?:  %s' % callback.isCallable())
+        self.callback.append(QJSValue(callback))
+        # self.callback = callback
+        # self.dump()
+
+    @pyqtSlot()
+    def processResponses(self):
+        print('processing responses')
+        self.dump()
+
+    @pyqtSlot(str)
+    def log(self, s):
+        print(s)
+
+
+print('Starting ICE Control GUI...')
+app = QApplication(sys.argv)
+view = QQuickView()
+context = view.rootContext()
+
+view.setSource(QUrl("app.qml"))
+if view.status() == QQuickView.Error:
+    sys.exit(-1)
+
+view.show()
+res = app.exec_()
+del view
+sys.exit(res)
